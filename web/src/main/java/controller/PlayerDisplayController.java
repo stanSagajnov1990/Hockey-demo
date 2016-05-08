fb56cd620fa0761c996bcb84f5066b91b5640d8e
Stanislav Sagajnov <stanislav.sagajnov.dev@gmail.com>
2016-05-08 14:10:32 +0200

    added PlayerStatistics for Player

--- web/src/main/java/controller/PlayerDisplayController.java
+++ web/src/main/java/controller/PlayerDisplayController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import java.util.List;
+
 import javax.naming.InitialContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +11,10 @@ import org.springframework.web.servlet.mvc.AbstractController;
 
 import com.stanislav.PlayerEJB;
 import com.stanislav.model.Player;
+import com.stanislav.model.PlayerStatistics;
 
-public class PlayerDisplayController extends AbstractController{
+public class PlayerDisplayController extends AbstractController {
 
-	
-	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest req,
 			HttpServletResponse resp) throws Exception {
@@ -23,6 +24,16 @@ public class PlayerDisplayController extends AbstractController{
 		PlayerEJB playerEJB = (PlayerEJB) context.lookup("java:app/Hockey-ejb/PlayerEJB");
 		Player player = playerEJB.getPlayerById(Long.valueOf(req.getParameter("id")));
 		model.addObject("player", player);
+		List<PlayerStatistics> playerStatistics = player.getPlayerStatistics();
+		
+		for(PlayerStatistics statistic : playerStatistics){
+			if (statistic.getYear() == 2015 && !statistic.isPlayoffStatistics()){
+				model.addObject("seasonStatistics", statistic);
+			}
+			if(statistic.getYear() == 2015 && statistic.isPlayoffStatistics()){
+				model.addObject("playoffsStatistics", statistic);
+			} 
+		}
 		
 		return model;
 	}
